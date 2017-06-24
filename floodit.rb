a4363c553ce2d2a32d391dfe4322336a861d775b
Daniel Anghel <srsdanitest@gmail.com>
2017-06-24 08:11:33 +0300

    updated menu with fileread score

--- floodit.rb
+++ floodit.rb
@@ -23,7 +23,28 @@ end
 
 # TODO: Implement everything else as described in the
 #       assignment brief.
+$scorr=File.read("savegame.txt").to_i
+$boardHeight=9
+$boardWidth=14
+def displayScore
+	if($scorr==0)
+		puts "No games Played yet."
+	else
+		puts "Best Game: #{$scorr} turns"
+	end
+end
+def mainMenu
+	system ("cls")
+	puts "Main menu:"
+	puts "s = Start game"
+	puts "c = Change size"
+	puts "q = Quit"
+	displayScore()
+	
+end
 
+def splashScreen
+system ("cls")
 splsh = ConsoleSplash.new(20,60)
 splsh.write_center(-3, "Apasa <enter> pentru a continua")
 splsh.write_horizontal_pattern("*",{:fg=>:white, :bg=>:green})
@@ -32,3 +53,9 @@ splsh.write_vertical_pattern("*",{:fg=>:white, :bg=>:red})
 splsh.write_header("Flood it", "Anghel Daniel", "1.0.0")
 splsh.splash
 gets()
+end
+
+
+splashScreen()
+
+mainMenu()
